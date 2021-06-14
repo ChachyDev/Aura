e753c2aba50afc3cd1ae05e4d52bba5a42711d71
ChachyDev <shalomadecoolboy@outlook.com>
2021-06-14 17:22:13 +0100

    Update "info" command to provide prefix(es) instead of available channels

--- modules/aura-core/src/main/kotlin/club/chachy/core/commands/InfoCommand.kt
+++ modules/aura-core/src/main/kotlin/club/chachy/core/commands/InfoCommand.kt
@@ -10,10 +10,12 @@ import club.chachy.utils.heart
 import club.chachy.utils.send
 import club.chachy.utils.separator
 import com.github.fcannizzaro.material.Colors
-import org.apache.commons.lang3.time.DurationFormatUtils
 import java.time.Instant
 
 fun CoreModule.info() = command("info") {
+    val prefixes = environment.prefixes
+    val plural = if (prefixes.size > 1) "es" else ""
+
     channel.send {
         setAuthor("Aura $separator Info", null, message.jda.selfUser.effectiveAvatarUrl)
         setColor(Colors.green_400.asColor())
@@ -22,7 +24,7 @@ fun CoreModule.info() = command("info") {
         field("$separator Version", Version.version, true)
         field("$separator Guilds", message.jda.guilds.size, true)
         field("$separator Users", message.jda.users.size, true)
-        field("$separator Channels", message.jda.textChannels.size, true)
+        field("$separator Prefix$plural", prefixes.joinToString(), true)
         setFooter("Powered by JDA and Love $heart", author.effectiveAvatarUrl)
         setTimestamp(Instant.now())
     }
